ff1c3f4580d779cc157d312980a700fa8309e5b0
Danny Yoo <dyoo@hashcollision.org>
2013-01-17 15:01:07 -0700

    fixing path lookup for platform check

--- lib/get-platform-lib.rkt
+++ lib/get-platform-lib.rkt
@@ -6,7 +6,8 @@
 (require ffi/unsafe
          racket/runtime-path
          (for-syntax racket/base
-                     racket/syntax))
+                     racket/syntax
+                     racket/match))
 
 ;; We case analyze on the value of system-library-subpath.
 ;; There are a few known strings (listed as DEFINE 
@@ -15,47 +16,39 @@
 (define-syntax (define-sdl-library-path stx)
   (with-syntax ([sdl-library-path
                  (format-id stx "sdl-library-path" #:source stx)])
-
-    (case (system-type 'os)
-      [(unix)
-       (cond
-	[(equal? "x86_64-linux" (system-library-subpath #f))
+    (define platform (path->string (system-library-subpath #f)))
+    (match platform
+	["x86_64-linux"
 	 #'(begin
              (log-debug "sdl: linux64")
 	     (define-runtime-path sdl-library-path
 	       (build-path "linux64" "libSDL-1.2.so.0.11.4")))]
-	[(equal? "i386-linux" (system-library-subpath #f))
+	["i386-linux"
 	 #'(begin
              (log-debug "sdl: linux32")
 	     (define-runtime-path sdl-library-path
 	       (build-path "linux32" "libSDL-1.2.so.0.11.4")))]
-        [else
-         (error 'define-sdl-library-path 
-                (format "Unsupported platform: ~a" 
-                        (system-library-subpath #f)))])]
-      [(windows)
-       (cond
-        [(equal? "win32\\x86_64" (system-library-subpath #f))
+        ["win32\\x86_64"
          #'(begin
              (log-debug "sdl: windows 64 bit")
              (define-runtime-path sdl-library-path
                (build-path "win64" "SDL.dll")))]
-        [(equal? "win32\\i386" (system-library-subpath #f))
+        ["win32\\i386"
           #'(begin
              (log-debug "sdl: windows 32 bit")
              (define-runtime-path sdl-library-path
                (build-path "win32" "SDL.dll")))]
+        [(or "x86_64-macosx" "i386-macosx")
+         #'(begin
+             (require "macosx/wait-for-sdl-init.rkt")
+             (log-debug "sdl: macosx")
+             (define-runtime-path sdl-library-path
+               (build-path "macosx" "SDL")))]
         [else
-         (error 'define-sdl-library-path 
-                (format "Unsupported platform: ~a" 
-                        (system-library-subpath #f)))])]
-
-      [(macosx)
-       #'(begin
-           (require "macosx/wait-for-sdl-init.rkt")
-           (log-debug "sdl: macosx")
-           (define-runtime-path sdl-library-path
-             (build-path "macosx" "SDL")))])))
+         (raise-syntax-error #f
+                             (format "Unsupported platform: ~a" 
+                                     (system-library-subpath #f))
+                             stx)])))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
